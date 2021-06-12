1534022881e1f7c01c4c99a83cf4aab5fbfbb6ab
Bandana Bharti <bhartibandana789@gmail.com>
2021-06-12 19:45:41 +0530

    Added: Validation part in Registartion Page

--- Registration/JS/RegValidation.js
+++ Registration/JS/RegValidation.js
@@ -1,7 +1,44 @@
 function validateforms(){
-    var x = document.forms["myform"]["name"];
-    if ( x == ""){
+
+   
+    let form = document.myform;
+
+    form.addEventListener("submit", (e)=>{
+        e.preventDefault();
+       });
+
+    if ( form.name.value == ""){
         alert("Name filled is required");
+        form.name.focus();
         return false;
     }
+    if( form.fname.value == ""){
+        alert("Please enter Father' Name");
+        form.fname.focus();
+        return ;
+    }
+
+    if( form.email.value == ""){
+        alert("Please enter Email id!");
+        form.email.focus();
+        return ;
+    }
+    if( form.address.value == ""){
+        alert("Please enter Address!");
+        form.address.focus();
+        return ;
+    }
+    if( form.phone.value == ""){
+        alert("Please enter Phone Number");
+        form.phone.focus();
+        return ;
+    }
+
+    if( form.password.value == ""){
+        alert("Please enter Password");
+        form.password.focus();
+        return ;
+    }
+    
+
 }
